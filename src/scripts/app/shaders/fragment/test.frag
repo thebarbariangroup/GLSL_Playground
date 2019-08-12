8351c6aee14553a4e8ca72267cea49e7f61637df
CCliff <clcliff30@gmail.com>
2019-08-12 16:27:34 -0400

    Just trying things

--- src/scripts/app/shaders/fragment/test.frag
+++ src/scripts/app/shaders/fragment/test.frag
@@ -1,14 +1,16 @@
-#define radius 7.
+// #define radius 7.
 #define numCircles 100
 
 uniform sampler2D uImage0;
 uniform vec3 uResolution;
 uniform float uTime;
 uniform vec4 data[numCircles];
+uniform float radius;
 
 varying vec2 vTexCoord;
 
 void main() {
+  float r = radius * uResolution.x;
   vec2 coords = vTexCoord * uResolution.xy;
 
   float d = 0.;
@@ -16,7 +18,7 @@ void main() {
     vec4 circle = data[i];
     circle.xy = circle.xy * uResolution.xy;
 
-    d = max(d, 1. - step(radius, distance(coords, circle.xy)));
+    d = max(d, 1. - step(r, distance(coords, circle.xy)));
   }
 
 
--- src/scripts/app/shaders/presets/_test.js
+++ src/scripts/app/shaders/presets/_test.js
@@ -4,9 +4,15 @@ import fs from '../fragment/_index';
 
 export default function ({ webcam, renderer }) {
   const circles = [];
+  const radius = 0.005;
 
   for (var i = 100; i >= 0; i--) {
-    const circle = new THREE.Vector4( Math.random(), Math.random(), (Math.random() - 0.5) * 0.01, (Math.random() - 0.5) * 0.01);
+    const circle = new THREE.Vector4( 
+      Math.random(), 
+      Math.random(), 
+      (Math.random() - 0.5) * 0.005, 
+      (Math.random() - 0.5) * 0.005
+    );
     circles.push(circle);
   }
 
@@ -19,9 +25,7 @@ export default function ({ webcam, renderer }) {
   });
 
   const outputBuffers = outputFactory.create([
-    {
-      id: 'o0',
-    },
+    {},
   ]);
 
   const outputBuffer = outputBuffers[0];
@@ -48,14 +52,28 @@ export default function ({ webcam, renderer }) {
           type: "v4v",
           value: circles,
         },
+        radius: {
+          _type: "object",
+          value: radius,
+        }
       },
       customUpdate: () => {
-        const radius = 0.005;
+        const dampening = 1.0;
+        const maxSpeed = 0.005;
+        const cof = 1.0;
+
+        // const randomIndexToAccelerate = Math.floor(Math.random() * circles.length);
+        // const circleToAccelerate = circles[randomIndexToAccelerate];
+        // circleToAccelerate.z *= Math.random() * 0.2 + 1;
+        // circleToAccelerate.w *= Math.random() * 0.2 + 1;
+
+        // circleToAccelerate.z = Math.min(Math.max(circleToAccelerate.z, -maxSpeed), maxSpeed);
+        // circleToAccelerate.w = Math.min(Math.max(circleToAccelerate.w, -maxSpeed), maxSpeed);
 
         for (var i = circles.length - 1; i >= 0; i--) {
           const circle = circles[i];
-          circle.x += circle.z;
-          circle.y += circle.w;
+          circle.x += circle.z * cof;
+          circle.y += circle.w * cof;
 
           if (circle.x - radius <= 0 || circle.x + radius >= 1) {
             circle.z *= -1;
@@ -75,10 +93,10 @@ export default function ({ webcam, renderer }) {
             if (distance <= radius * 2) {
               // Formula is (Xv1 * (mass1 - mass2) + (2 * mass2 * Xv2)) / (mass1 + mass2);
               // Since we are sharing the same mass, we can cut out part of the formula for optimization
-              const circleZ = ((2 * radius * localCircle.z)) / (radius + radius);
-              const circleW = ((2 * radius * localCircle.w)) / (radius + radius);
-              const localCircleZ = ((2 * radius * circle.z)) / (radius + radius);
-              const localCircleW = ((2 * radius * circle.w)) / (radius + radius);
+              const circleZ = ((2 * radius * localCircle.z)) / (radius + radius) * dampening;
+              const circleW = ((2 * radius * localCircle.w)) / (radius + radius) * dampening;
+              const localCircleZ = ((2 * radius * circle.z)) / (radius + radius) * dampening;
+              const localCircleW = ((2 * radius * circle.w)) / (radius + radius) * dampening;
               
               circle.z = circleZ;
               circle.w = circleW;
